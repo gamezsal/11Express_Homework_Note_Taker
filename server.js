08628e9b761255d57c29f60f1ae26ea29d66a4e5
Sal Gamez <gamezsalvador@gmail.com>
2020-11-27 21:48:27 -0700

    added required html routes and added routes folder with html and api routes .js file

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log("Server listening on: http://localhost:" + PORT);
