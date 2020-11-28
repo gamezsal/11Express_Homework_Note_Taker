8094eec4e3802d4d5298f2cbfaaa3a4d506ce8d8
Sal Gamez <gamezsalvador@gmail.com>
2020-11-27 21:58:56 -0700

    added required apiroutes file to server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
