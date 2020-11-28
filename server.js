f8fd2f027ad6163880ec150cd3469f6025d558e7
Sal Gamez <gamezsalvador@gmail.com>
2020-11-27 21:28:19 -0700

    added server.js and modified package.json for node start script for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
